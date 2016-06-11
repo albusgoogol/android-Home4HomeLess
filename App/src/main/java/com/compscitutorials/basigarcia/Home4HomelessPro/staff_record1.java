a8548a95efad33d2dea4466f663c164f79f8ea75
WeRockStar <vr_freestyle@outlook.com>
2016-06-11 23:45:04 +0700

    Fixed bug date picker

--- App/src/main/java/com/compscitutorials/basigarcia/Home4HomelessPro/staff_record1.java
+++ App/src/main/java/com/compscitutorials/basigarcia/Home4HomelessPro/staff_record1.java
@@ -1,5 +1,6 @@
 package com.compscitutorials.basigarcia.Home4HomelessPro;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -15,13 +16,16 @@ import android.text.Editable;
 import android.text.Html;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.Filterable;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -57,6 +61,8 @@ public class staff_record1 extends AppCompatActivity implements View.OnClickList
     private static final LatLngBounds BOUNDS_MOUNTAIN_VIEW = new LatLngBounds(
             new LatLng(37.398160, -122.180831), new LatLng(37.430610, -121.972090));
 
+    private RelativeLayout rootLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,6 +70,8 @@ public class staff_record1 extends AppCompatActivity implements View.OnClickList
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        rootLayout = (RelativeLayout) findViewById(R.id.rootLayout);
+        rootLayout.setOnClickListener(this);
 
         Spinner dropdown = (Spinner) findViewById(R.id.spinner1);
         String[] items = new String[]{"คนเร่ร่อน", "คนไร้บ้าน", "ขอทาน", "ผู้ยากไร้"};
@@ -92,11 +100,9 @@ public class staff_record1 extends AppCompatActivity implements View.OnClickList
         // BD
 
         dateEditText = (EditText) findViewById(R.id.bdedittext);
-        // check if picker mode is specified in Style.xml
 
-        dateEditText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        dateEditText.setOnTouchListener((v, event) -> {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
                 Calendar now = Calendar.getInstance();
                 DatePickerDialog dpd = DatePickerDialog.newInstance(
                         staff_record1.this,
@@ -104,22 +110,13 @@ public class staff_record1 extends AppCompatActivity implements View.OnClickList
                         now.get(Calendar.MONTH),
                         now.get(Calendar.DAY_OF_MONTH)
                 );
+                if (dpd != null) dpd.setOnDateSetListener(this);
                 dpd.show(getFragmentManager(), "Datepickerdialog");
             }
+            return false;
         });
 
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        DatePickerDialog dpd = (DatePickerDialog) getFragmentManager().findFragmentByTag("Datepickerdialog");
-
-
-        if (dpd != null) dpd.setOnDateSetListener(this);
-    }
-
 
     private AdapterView.OnItemClickListener mAutocompleteClickListener
             = new AdapterView.OnItemClickListener() {
@@ -185,6 +182,10 @@ public class staff_record1 extends AppCompatActivity implements View.OnClickList
                 Intent i = new Intent(this, staff_record2.class);
                 startActivity(i);
                 break;
+            case R.id.rootLayout:
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(rootLayout.getWindowToken(), 0);
+                break;
         }
     }
 
@@ -192,6 +193,7 @@ public class staff_record1 extends AppCompatActivity implements View.OnClickList
     public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
         String date = +dayOfMonth + "/" + (++monthOfYear) + "/" + year;
         dateEditText.setText(date);
+        view.dismiss();
     }
 }
 
